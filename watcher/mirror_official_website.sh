e41952881438019138bc5c0078ec53059de15bcf
Shuhei Takahashi <takahashi.shuhei@gmail.com>
2017-08-04 19:56:45 +0900

    Script to mirror the official site.

---
+++ watcher/mirror_official_website.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+cd "$(dirname "$0")/.."
+
+set -x
+
+while :; do
+    git reset
+    git clean -fd events.inf.ed.ac.uk/
+    git pull
+    wget -m -p 'http://events.inf.ed.ac.uk/icfpcontest2017/'
+    find events.inf.ed.ac.uk/ -name '*.[123456789]'  -delete
+    git add events.inf.ed.ac.uk/
+    if git commit -m 'Snapshot of the official site.' events.inf.ed.ac.uk/; then
+        if ! git push; then
+            git reset HEAD~
+        fi
+    fi
+    sleep 1m
+done
