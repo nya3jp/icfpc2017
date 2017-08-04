ebaf6da33a595d3b5ecfae429190780e544f9512
Shuhei Takahashi <takahashi.shuhei@gmail.com>
2017-08-04 20:45:42 +0900

    Revert "Handle wget failure."
    This reverts commit 0f21d58250e467ef1784c8a62d8a3c1ee4b57d94.

--- watcher/mirror_official_website.sh
+++ watcher/mirror_official_website.sh
@@ -8,13 +8,12 @@ while :; do
     git reset
     git clean -fd events.inf.ed.ac.uk/
     git pull
-    if wget -m -p 'http://events.inf.ed.ac.uk/icfpcontest2017/'; then
-        find events.inf.ed.ac.uk/ -name '*.[123456789]'  -delete
-        git add events.inf.ed.ac.uk/
-        if git commit -m 'Snapshot of the official site.' events.inf.ed.ac.uk/; then
-            if ! git push; then
-                git reset HEAD~
-            fi
+    wget -m -p 'http://events.inf.ed.ac.uk/icfpcontest2017/'
+    find events.inf.ed.ac.uk/ -name '*.[123456789]'  -delete
+    git add events.inf.ed.ac.uk/
+    if git commit -m 'Snapshot of the official site.' events.inf.ed.ac.uk/; then
+        if ! git push; then
+            git reset HEAD~
         fi
     fi
     sleep 1m
