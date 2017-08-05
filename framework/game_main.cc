b7109e0299b89f52d992fe4574525d7edafccffa
irori <irorin@gmail.com>
2017-08-05 14:25:11 +0900

    Add --punter flag

--- framework/game_main.cc
+++ framework/game_main.cc
@@ -4,16 +4,26 @@
 #include "base/logging.h"
 
 #include "framework/game.h"
+#include "framework/pass_punter.h"
 #include "punter/random_punter.h"
 
 using namespace framework;
 using namespace punter;
+
+DEFINE_string(punter, "RandomPunter", "Punter class.");
 
 int main(int argc, char* argv[]) {
   gflags::ParseCommandLineFlags(&argc, &argv, true);
   google::InitGoogleLogging(argv[0]);
   google::InstallFailureSignalHandler();
 
-  Game game(base::MakeUnique<RandomPunter>());
+  std::unique_ptr<Punter> punter;
+  if (FLAGS_punter == "RandomPunter")
+    punter = base::MakeUnique<RandomPunter>();
+  else if (FLAGS_punter == "PassPunter")
+    punter = base::MakeUnique<PassPunter>();
+  else
+    LOG(FATAL) << "invalid punter name";
+  Game game(std::move(punter));
   game.Run();
 }
