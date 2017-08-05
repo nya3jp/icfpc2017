a28b54a1436c0975bbe2461d81fdb5f7d2aca9f8
phoenixstarhiro <phoenixstarhiro@users.noreply.github.com>
2017-08-05 14:33:05 +0900

    Add PUNTER_TYPE

--- framework/run.sh
+++ framework/run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-exec bazel-bin/framework/game_main --name "${PUNTER_NAME}" --logtostderr
+exec bazel-bin/framework/game_main --name "${PUNTER_NAME}" --punter="${PUNTER_TYPE}" --logtostderr
