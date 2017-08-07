91314ef976d9a2d80e8e34f2accbc21787444a9a
Yu Sugawara <gusmachine+git@gmail.com>
2017-08-07 06:00:07 +0000

    Add test impl of switching punter.

--- punter/punter_factory.cc
+++ punter/punter_factory.cc
@@ -45,10 +45,43 @@ std::unique_ptr<framework::Punter> PunterByName(const std::string& name) {
     return base::MakeUnique<MetaPunter>();
   else if (name == "FriendlyPunter")
     return base::MakeUnique<FriendlyPunter>();
+  else if (name == "BenkeiOrJam")
+    return base::MakeUnique<SwitchingPunter>(SwitchingPunter::BenkeiOrJam);
   else
     LOG(FATAL) << "invalid punter name: " << name;
   return nullptr;
 }
+
+// SwitchingPunter.
+SwitchingPunter::SwitchingPunter(SwitchingPunter::ChooserType f)
+  : f_(f) {}
+SwitchingPunter::~SwitchingPunter() = default;
+
+void SwitchingPunter::SetUp(const common::SetUpData& args) {
+  chosen_ = PunterByName(f_(args));
+  chosen_->SetUp(args);
+}
+
+framework::GameMove SwitchingPunter::Run(
+    const std::vector<framework::GameMove>& moves) {
+  return chosen_->Run(moves);
+}
+
+void SwitchingPunter::SetState(std::unique_ptr<base::Value> state_in) {
+  chosen_->SetState(std::move(state_in));
+}
+
+std::unique_ptr<base::Value> SwitchingPunter::GetState() {
+  return chosen_->GetState();
+}
+
+// static
+std::string SwitchingPunter::BenkeiOrJam(const common::SetUpData& args) {
+  if (args.num_punters == 2 &&
+      args.game_map.sites.size() <  100) { return "GreedyToJam"; }
+  return "Benkei";
+}
+
 
 }  // namespace punter
 
--- punter/punter_factory.h
+++ punter/punter_factory.h
@@ -7,6 +7,26 @@
 
 namespace punter {
 std::unique_ptr<framework::Punter> PunterByName(const std::string& name);
+
+class SwitchingPunter : public framework::Punter {
+public:
+  using ChooserType = std::function<std::string(const common::SetUpData&)>;
+  explicit SwitchingPunter(ChooserType f);
+  ~SwitchingPunter() override;
+
+  void SetUp(const common::SetUpData& args) override;
+  framework::GameMove Run(const std::vector<framework::GameMove>& moves) override;
+  void SetState(std::unique_ptr<base::Value> state) override;
+  std::unique_ptr<base::Value> GetState() override;
+
+  static std::string BenkeiOrJam(const common::SetUpData& args);
+ private:
+  ChooserType f_;
+  std::unique_ptr<framework::Punter> chosen_;
+
+  DISALLOW_COPY_AND_ASSIGN(SwitchingPunter);
+};
+
 }  // namespace punter
 
 #endif  // PUNTER_PUNTER_FACTORY_H__
